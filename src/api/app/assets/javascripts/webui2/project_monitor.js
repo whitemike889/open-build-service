0ba938fe9e6377898b5ce7ad73a555f98af78f4e
Saray Cabrera Padrón <scabrerapadron@suse.de>
2019-02-13 17:28:05 +0100

    DataTable rendering on Monitor page
    The content of the cell is now processed and rendered by JavaScript.
    This improves the performance of the DataTable initialization and it is
    needed for the upcoming server-side pagination.
    
    For example, with openSUSE:Tools (74 packages) there is a difference of
    approximately 2 seconds.
    
    Co-authored-by: Ana María Martínez Gómez <anamaria@martinezgomez.name>

--- src/api/app/assets/javascripts/webui2/project_monitor.js
+++ src/api/app/assets/javascripts/webui2/project_monitor.js
@@ -6,17 +6,73 @@ function setAllLinks(event) {
   $(this).closest('.dropdown-menu').find('input').prop('checked', event.data.checked);
 }
 
+function statusCell(packageName, meta, statusHash, tableInfo, project) {
+  var info = tableInfo[meta.col-1];
+  var repository = info[0];
+  var architecture = info[1];
+  var status = statusHash[repository][architecture][packageName] || {};
+  var code = status.code;
+  if (code === undefined) { return null; }
+
+  var id = meta.row + '-' + meta.col;
+  var details = status.details;
+  var output = '<a ';
+  var klass = 'build-state-' + code;
+
+  if (['succeeded', 'failed', 'building'].includes(code)) {
+    var url = '/package/live_build_log/' + project + '/' + packageName + '/' + repository + '/' + architecture;
+    output += 'href="' + url + '"';
+  }
+  else {
+    output += 'href="javascript:void(0);"';
+    output += ' id="' + id + '"';
+
+    if (details !== undefined) {
+      if (code === 'scheduled') klass = 'text-warning';
+      output += ' data-content="' + details + '" data-placement="right" data-toggle="popover"';
+    }
+  }
+  output += ' class="' + klass + '">' + code + '</a>';
+  return output;
+}
+
 function setupProjectMonitor() { // jshint ignore:line
+  var data = $('tbody').data();
+  var packageNames = data.packagenames;
+  var statusHash =data.statushash;
+  var tableInfo = data.tableinfo;
+  var project = data.project;
+
   initializeDataTable('#project-monitor-table', { // jshint ignore:line
     scrollX: true,
     fixedColumns: true,
     pageLength: 50,
     lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
+    data: packageNames,
     search: {
       regex: true,
       smart: false,
     },
-    columnDefs: [{ width: 150, targets: 0 }]
+    columnDefs: [
+      {
+        width: 150,
+        targets: 0,
+        className: 'text-left',
+        data: null,
+        render: function (packageName) {
+          var url = '/package/show/' + project + '/' + packageName;
+          return '<a href="' + url + '">' + packageName + '</a>';
+        }
+      },
+      {
+        targets: '_all',
+        data: null,
+        className: 'text-center',
+        render: function (packageName, type, row, meta) {
+          return statusCell(packageName, meta, statusHash, tableInfo, project);
+        }
+      }
+    ]
   });
 
   $('#table-spinner').addClass('d-none');
